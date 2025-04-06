a54d49a28393857572a2896a1603bf14571611e8
arjonbnp <tahmidorjon@gmail.com>
2025-04-07 02:55:49 +0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Political Crime Report Viewer!");
+        System.out.println("Powered by Java + HTML + Google Sheets");
+    }
+}
